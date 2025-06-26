4a75d4bde1f3b5af312596c6d2fa4cf63508dbc9
Juan <jc454591@gmail.com>
2025-06-25 23:14:52 -0300

    feat: update login view to use CustomLoginView and add registration path

--- order_manager/urls.py
+++ order_manager/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from orders.views import register, CustomLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='orders/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', register, name='register'),
     path('', include('orders.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
